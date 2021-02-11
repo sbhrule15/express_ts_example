e77dc6072d49247eda8356e69c088a22ccec9f0c
Joshua Pare <65102852+sbhrule15@users.noreply.github.com>
2021-02-11 13:29:01 -0500

    fixing type definition file

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -2,8 +2,28 @@ import { Router, Request, Response } from 'express';
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
-  res.send('hi there')
+router.get('/login', (req: Request, res: Response) => {
+  res.send(`
+  <form method="POST">
+    <div>
+      <label>Email</label>
+      <input name="email" />
+    </div>
+    <div>
+      <label>Password</label>
+      <input name="password" type="password"/>
+    </div>
+    <button>Submit</button>
+  </form> `)
+})
+
+router.post('/login', (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  if (email) {
+    res.send(email.toUpperCase());
+  }
+  
 })
 
 export { router };
